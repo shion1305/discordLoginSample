8ebcd9a19a56b28a1214d514805bd8f40c5ef411
Shion Ichikawa <shion1305@gmail.com>
2022-08-09 04:21:45 +0900

    ✨ basic code for OAuth2 authentication

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
@@ -19,9 +19,10 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,52 @@
-package discordLoginSample
+package main
 
 import (
 	"fmt"
-	"github.com/bwmarrin/discordgo"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 )
 
+var dEndpoint = oauth2.Endpoint{
+	AuthURL:   "https://discord.com/api/oauth2/authorize",
+	TokenURL:  "https://discord.com/api/oauth2/token",
+	AuthStyle: oauth2.AuthStyleInParams,
+}
+
 func main() {
-	dg, err := discordgo.New("Bot " + os.Getenv("discord_token"))
-	err = dg.Open()
-	if err != nil {
-		fmt.Println("error creating Discord session,", err)
+	//authUrl := "https://discord.com/api/oauth2/authorize?client_id=933021504319422544&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2FtestLogin&response_type=code&scope=identify%20email"
+	engine := gin.Default()
+	engine.GET("/testLogin", func(context *gin.Context) {
+		code := context.Query("code")
+		fmt.Println(code)
+		getUser(code, context)
+	})
+	engine.Run(":8080")
+}
+
+func getUser(code string, context *gin.Context) {
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("client_id"),
+		ClientSecret: os.Getenv("client_secret"),
+		Endpoint:     dEndpoint,
+		RedirectURL:  "http://localhost:8080/testLogin",
+		Scopes:       []string{"email", "identity"},
 	}
-	dg.AddHandler(onMessageCreate)
-	stopBot := make(chan os.Signal, 1)
-
-	signal.Notify(stopBot, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-
-	<-stopBot
+	token, err := conf.Exchange(context.Request.Context(), code)
+	if err != nil {
+		context.Writer.WriteHeader(http.StatusInternalServerError)
+		context.Writer.Write([]byte(err.Error()))
+		return
+	}
+	res, err := conf.Client(context.Request.Context(), token).Get("https://discord.com/api/users/@me")
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		context.Writer.WriteHeader(http.StatusInternalServerError)
+		context.Writer.Write([]byte(err.Error()))
+		return
+	}
+	context.Writer.Write(body)
 }
